1cde6ab04ebea152b921b2e3ed3dcbdddc1ef723
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-11-12 18:08:43 +0100

    first commit

---
+++ src/main/java/fr/hndgy/testcrystal/TestCrystalApplication.java
@@ -0,0 +1,13 @@
+package fr.hndgy.testcrystal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrystalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCrystalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/hndgy/testcrystal/TestCrystalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hndgy.testcrystal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCrystalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
